a8c92f68cdd3167f87eb712857c723785b86d6b3
Modesty Ophelia Aguilar <ophelia.m.aguilar@gmail.com>
2022-02-08 20:02:46 +0100

    update
    update

--- QuizList.js
+++ QuizList.js
@@ -1,6 +1,5 @@
 const QuizQuestions = [
   {
-    id: 0,
     question: "What sounds the most intriguing in a game?",
     hellBlade: "A: Action, combat and puzzles", //Hell Blade: Senuas Sacrifice
     rakuen: "B: Visiting magical worlds", //Rakuen
@@ -8,7 +7,6 @@ const QuizQuestions = [
     bioshockInfinite: "D: An amazing soundtrack", //Bioshock Infinite
   },
   {
-    id: 1,
     question: "How much lifespan are you hoping to get out of a game?",
     hellBlade: "A: I prefer a short but intense game for the duration",
     rakuen: "B: Something short I can pick up and drop whenever I feel like it",
@@ -18,7 +16,6 @@ const QuizQuestions = [
       "D: I prefer a longer game that captivates me from start to finish",
   },
   {
-    id: 2,
     question: "What type of graphics do you prefer?",
     hellBlade: "A: Immersive, terrifying horror graphics",
     rakuen: "B: Cute pixel art",
@@ -26,7 +23,6 @@ const QuizQuestions = [
     bioshockInfinite: "D: Spectacular, colorful graphics with beautiful views",
   },
   {
-    id: 3,
     question: "What artistic theme do you prefer in a game?",
     hellBlade: "A: Terrifying horror",
     rakuen: "B: Anime",
@@ -34,7 +30,6 @@ const QuizQuestions = [
     bioshockInfinite: "D: Steampunk",
   },
   {
-    id: 4,
     question: "What playstyle do you prefer in a game?",
     hellBlade: "A: Hack 'n slash",
     rakuen: "B: Roleplay",
